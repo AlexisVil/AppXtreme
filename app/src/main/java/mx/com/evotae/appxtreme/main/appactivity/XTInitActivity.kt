2a5b8903dc6261dde3624907465b9386e235c127
AlexisVil <alexisvilchiz@gmail.com>
2022-08-25 13:32:37 -0500

    DatePicker Dialog

--- app/src/main/java/mx/com/evotae/appxtreme/main/appactivity/XTInitActivity.kt
+++ app/src/main/java/mx/com/evotae/appxtreme/main/appactivity/XTInitActivity.kt
@@ -2,18 +2,11 @@ package mx.com.evotae.appxtreme.main.appactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationView
 import mx.com.evotae.appxtreme.R
 import mx.com.evotae.appxtreme.databinding.ActivityXtinicioBinding
-import mx.com.evotae.appxtreme.framework.util.commons.managerevents.DatePickerFragment
 
 class XTInitActivity : AppCompatActivity() {
     private lateinit var binding: ActivityXtinicioBinding
--- app/src/main/java/mx/com/evotae/appxtreme/main/reportarpago/ui/XTReportarPagoFragment.kt
+++ app/src/main/java/mx/com/evotae/appxtreme/main/reportarpago/ui/XTReportarPagoFragment.kt
@@ -1,19 +1,20 @@
 package mx.com.evotae.appxtreme.main.reportarpago.ui
 
 import android.app.Activity
+import android.app.DatePickerDialog
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.FragmentManager
-import com.bumptech.glide.manager.SupportRequestManagerFragment
+import android.widget.DatePicker
 import kotlinx.android.synthetic.main.fragment_reportar_pago.*
+import mx.com.evotae.appxtreme.R
 import mx.com.evotae.appxtreme.databinding.FragmentReportarPagoBinding
 import mx.com.evotae.appxtreme.framework.base.XTFragmentBase
-import mx.com.evotae.appxtreme.framework.util.commons.managerevents.DatePickerFragment
+import java.text.SimpleDateFormat
+import java.util.*
 
-@Suppress("DEPRECATION")
 class XTReportarPagoFragment : XTFragmentBase() {
     lateinit var binding: FragmentReportarPagoBinding
     private lateinit var safeActivity: Activity
@@ -35,18 +36,32 @@ class XTReportarPagoFragment : XTFragmentBase() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        etFecha.setOnClickListener { showDatePickerDialog() }
-    }
 
-    private fun showDatePickerDialog() {
-        val datePicker = DatePickerFragment { day, month, year -> onDateSelected(day, month, year) }
-        parentFragmentManager?.let {manager ->
-            datePicker.show(manager, "datePicker")
+
+
+        binding.apply {
+            etFecha.setOnClickListener {
+                createDatePicker()
+            }
         }
     }
 
-    fun onDateSelected(day: Int, month: Int, year: Int){
-        etFecha.setText("$day / $month / $year")
+    private fun createDatePicker(){
+        val c = Calendar.getInstance()
+        val datePicker = DatePickerDialog.OnDateSetListener { view: DatePicker?, year: Int, month: Int, dayOfMonth: Int ->
+            c.set(Calendar.YEAR, year)
+            c.set(Calendar.MONTH, month)
+            c.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateLabel(c)
+        }
+        DatePickerDialog(safeActivity,R.style.AppTheme_DatePickerDialog, datePicker, c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH)).show()
     }
+
+    private fun updateLabel(c: Calendar) {
+        val calenderFormat="dd/MM/yy"
+        val sdf = SimpleDateFormat(calenderFormat, Locale.ROOT)
+        etFecha.setText(sdf.format(c.timeInMillis))
+    }
+
 
 }
